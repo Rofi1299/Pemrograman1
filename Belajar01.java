74dc14d0dfad586b2d95cb6f19ef84c457352a11
Rofi1299 <31511741+Rofi1299@users.noreply.github.com>
2017-09-05 11:41:03 +0700

    Add files via upload

---
+++ Belajar01.java
@@ -0,0 +1,7 @@
+public class Belajar01 {
+    public static void main(String[] args) {
+        System.out.println("Nama : MUHAMAD ROFIUDIN");
+        System.out.println("Nim : 17220041");
+        
+    }
+}
